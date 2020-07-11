48c18e7aabce4f9d0f9a9ff028c72bf595190446
Axel Rindle <mail@axelrindle.de>
2020-07-11 17:31:13 +0200

    Hook into npm version

---
+++ bump-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Bash Script to be run as npm's postversion hook.
+#
+# The CHANGELOG.md file will be regenerated.
+# Changes to this file and the both package files are committed.
+#
+# Nothing will be pushed to allow for manual review.
+#
+# # #
+
+VERSION=$(cat package.json | jq -r .version)
+echo " >  Temporarily createing new tag ($VERSION) ..."
+git tag $VERSION
+
+echo " >  Updating CHANGELOG.md ..."
+node ./lib/ --paths res,lib
+echo
+git add CHANGELOG.md
+git commit -m "Update CHANGELOG.md"
+
+echo " >  Committing package files ..."
+git add package.json package-lock.json
+git commit -m "Version $VERSION :tada:"
+
+echo " >  Forcing an update of the existing tag ..."
+git tag -f $VERSION
+
+echo " >  Done."
